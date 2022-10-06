3d8eeb3cd6eeb3866704f5dacf8877d0f968c340
Cheenu Khurana <cheenu.ck@gmail.com>
2022-10-06 15:45:36 +0530

    membership types thumbnails

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
+import MembershipTypes from '../components/membershipTypes'
 
 export default function Home() {
   return (
     <Layout>
-      Start your membership today
+      Choose Your All Pass
+      <MembershipTypes />
     </Layout>
   )
 }
