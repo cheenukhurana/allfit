0aa0091b305185955571afbf27ec5f01d84df803
Cheenu Khurana <cheenu.ck@gmail.com>
2022-10-08 16:10:35 +0530

    handle buy membership fake

--- pages/memberships/[duration].js
+++ pages/memberships/[duration].js
@@ -1,13 +1,37 @@
 import Layout from "../../components/layout";
 import { membershipTypesData } from "../../membershipTypesData";
+import { useAuth } from "../../utils/authProvider";
+
+const buyMembership = async () => {
+    console.log("Waiting for transaction to happen")
+    var prom = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, 3000)
+    })
+    return prom
+}
 
 export default function MembershipType({ membershipTypeData }) {
+
+    const { currentAccount } = useAuth()
+
+    const handleBuyNow = () => {
+        if(currentAccount){
+            buyMembership()
+            .then(() => console.log("Transaction complete")) 
+        }
+        else {
+            alert("Please connect your wallet")
+        }
+    }
 
     return (
         <Layout>
             {membershipTypeData.duration} months
             <img className="w-[14rem] border border-gray-400" src={`/membershipNFTs/${membershipTypeData.duration}months.png`} alt=""></img>
             <p className="mt-4">You will be minted this nft upon buying</p>
+            <button className="mt-2 px-2 py-1 bg-white text-orange-500 rounded border border-gray-400" onClick={handleBuyNow}>BUY NOW</button>
         </Layout>
     )
 }
