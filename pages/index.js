510ca72998782ad85e52cdb39a8a0e04f30017cb
Cheenu Khurana <cheenu.ck@gmail.com>
2022-11-05 23:08:58 +0530

    remove unused dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
 import MembershipTypes from '../components/membershipTypes'
 
 export default function Home() {
