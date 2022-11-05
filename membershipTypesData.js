6b5295421fe65d7ab31c6762e9af1a81b277d9cf
Cheenu Khurana <cheenu.ck@gmail.com>
2022-11-06 02:06:31 +0530

    remove unused

--- membershipTypesData.js
+++
@@ -1,6 +0,0 @@
-export const membershipTypesData = [
-    {duration:"12", price:"250", extras:"Free 3 months extension"},
-    {duration:"6", price:"200", extras:"Free 2 months extension"},
-    {duration:"3", price:"150", extras:"Free 1 months extension"},
-    {duration:"1", price:"100", extras:"Free 10 days extension"}
-]
