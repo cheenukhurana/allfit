4131f8b659c5c1bb6b534fdb602b717591911c63
Sagar Anand <sagar.anand015@gmail.com>
2022-11-07 22:42:10 +0530

    added ipfs constants

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -6,6 +6,6 @@ export const STORE_CONTRACT_ADDRESS = "0x9e85e2Ee2D83a14f55C258e9a4012D628fD63c4
 export const NFT_CONTRACT_ADDRESS = "0x83Ef4328E745e1aC0A45C229b699dc6676556B1F"
 
 export const OneMonthsImg = "ipfs://bafybeigzbl6gipt4pyacg5zw72qfnwx7wb27t2rcusyyfjfy2aiqkttoue";
-export const SixMonthsImg = "";
-export const NineMonthsImg = "";
-export const TwelveMonthsImg = "";
+export const ThreeMonthsImg = "ipfs://bafybeig3lqldt2ikpqrqtu26uodvvjokvp34w5k6vsl3wrz6ikjl5x3w3e";
+export const SixMonthsImg = "ipfs://bafybeihiju6bbkih2xjs2avllntnpvpi6unb3lxna3nsuzq7lnfjikckoi";
+export const TwelveMonthsImg = "ipfs://bafybeicuiumf2vd37olgze2lvqax3ne74c7owsdwinflmlyju376ycneqy";
