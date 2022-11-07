021a9a1c8289da57aef1589a69bcc70dd1de65ed
Cheenu Khurana <cheenu.ck@gmail.com>
2022-11-07 21:56:08 +0530

    cover pic and description

---
+++ frontend/components/cover.js
@@ -0,0 +1,25 @@
+
+const Cover = () => {
+
+    return (
+        <div className="flex justify-between">
+            <div className="flex-1">
+                <div className=" m-20 max-w-fit boder">
+                    <p className="text-xl text-orange-500">All Pass</p>
+                    <p className="mt-6 text-3xl font-bold">Unlimited Access to</p>
+                    <ul class="mt-6 list-disc list-inside">
+                        <li >All Gym Centers</li>
+                        <li className="mt-4">At-Centere Group Classes</li>
+                        <li className="mt-4">At-home Live Workouts</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="flex-1">
+                <img src="/coverPic.png" className="block mx-auto align-right w-[60%] text-right" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Cover
