c73a766031037d6ff0af9a30a21ed2858a63ec12
Cheenu Khurana <cheenu.ck@gmail.com>
2022-11-07 23:18:58 +0530

    update constants

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -5,7 +5,9 @@ export const COVALENT_API_KEY = "ckey_67ab6a60325149cc85edc08efbd";
 export const STORE_CONTRACT_ADDRESS = "0xfDB312D8614EB0676b955a9cf6019403af373228"
 export const NFT_CONTRACT_ADDRESS = "0x629D77170a87baD851212D084d7aFaC2A5ADBF37"
 
-export const OneMonthsImg = "ipfs://bafybeigzbl6gipt4pyacg5zw72qfnwx7wb27t2rcusyyfjfy2aiqkttoue";
-export const ThreeMonthsImg = "ipfs://bafybeig3lqldt2ikpqrqtu26uodvvjokvp34w5k6vsl3wrz6ikjl5x3w3e";
-export const SixMonthsImg = "ipfs://bafybeihiju6bbkih2xjs2avllntnpvpi6unb3lxna3nsuzq7lnfjikckoi";
-export const TwelveMonthsImg = "ipfs://bafybeicuiumf2vd37olgze2lvqax3ne74c7owsdwinflmlyju376ycneqy";
+export const ipfsURLs = {
+    1: "ipfs://bafybeigzbl6gipt4pyacg5zw72qfnwx7wb27t2rcusyyfjfy2aiqkttoue",
+    3: "ipfs://bafybeigzbl6gipt4pyacg5zw72qfnwx7wb27t2rcusyyfjfy2aiqkttoue",
+    6: "ipfs://bafybeihiju6bbkih2xjs2avllntnpvpi6unb3lxna3nsuzq7lnfjikckoi",
+    12: "ipfs://bafybeihiju6bbkih2xjs2avllntnpvpi6unb3lxna3nsuzq7lnfjikckoi"
+}
