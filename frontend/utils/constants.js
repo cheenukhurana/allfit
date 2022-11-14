abf9334fa7cc75026092121bd46029a68a552a58
Cheenu Khurana <cheenu.ck@gmail.com>
2022-11-14 17:32:06 +0530

    change covalent to alchemy to read nft data

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -2,8 +2,10 @@ export const POLYGON_MUMBAI_RPC_LINK = "https://matic-mumbai.chainstacklabs.com"
 export const CHAIN_ID = 80001
 export const COVALENT_BASE_URL = "https://api.covalenthq.com/v1/";
 export const COVALENT_API_KEY = "ckey_67ab6a60325149cc85edc08efbd";
+export const ALCHEMY_BASE_URL = "https://polygon-mumbai.g.alchemy.com/nft/v2/";
+export const ALCHEMY_API_KEY = "ZGSPs61MyX4pIjBOi14TS1ymlisFjdZs";
 export const STORE_CONTRACT_ADDRESS = "0xfDB312D8614EB0676b955a9cf6019403af373228"
-export const NFT_CONTRACT_ADDRESS = "0x629D77170a87baD851212D084d7aFaC2A5ADBF37"
+export const NFT_CONTRACT_ADDRESS = "0x629d77170a87bad851212d084d7afac2a5adbf37"
 export const NFT_CONTRACT_ADDRESS_TEST = "0xa07e45a987f19e25176c877d98388878622623fa"
 
 export const ipfsURLs = {
